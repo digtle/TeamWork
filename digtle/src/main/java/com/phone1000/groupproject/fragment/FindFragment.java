df248e62e5abc758954265c2e590abdc9f58a476
wangwei2891787465 <2891787465@qq.com>
2016-09-18 17:51:11 +0800

    Merge branch 'master' of E:\Users\Team with conflicts.

--- digtle/src/main/java/com/phone1000/groupproject/fragment/FindFragment.java
+++ digtle/src/main/java/com/phone1000/groupproject/fragment/FindFragment.java
@@ -161,22 +161,7 @@ private int dp2pix(int size){
         bannerAdapter = new ViewPagerAdapter();
         findBanner.setAdapter(bannerAdapter);
         mHandler.sendEmptyMessage(1);
-        findBanner.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
     }
 
     private void initView() {
